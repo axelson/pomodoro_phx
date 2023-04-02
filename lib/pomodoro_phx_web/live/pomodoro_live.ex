75d7535717f0acb74ab95636f9210720e0ec2820
Jason Axelson <jason.axelson@gmail.com>
2023-04-02 10:22:28 -1000

    Nicer LiveView

--- lib/pomodoro_phx_web/live/pomodoro_live.ex
+++ lib/pomodoro_phx_web/live/pomodoro_live.ex
@@ -3,18 +3,51 @@ defmodule PomodoroPhxWeb.PomodoroLive do
   require Logger
   alias Pomodoro.PomodoroTimer
 
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     if connected?(socket) do
       PomodoroTimer.register(self())
     end
 
-    socket = assign(socket, seconds_remaining: 0)
+    socket = assign(socket, timer: PomodoroTimer.get_timer())
 
     {:ok, socket}
   end
 
+  @impl Phoenix.LiveView
+  def handle_event("start", _value, socket) do
+    result = PomodoroTimer.start_ticking()
+    Logger.debug("Starting: #{inspect result}")
+    {:noreply, socket}
+  end
+
+  def handle_event("reset", _value, socket) do
+    result = PomodoroTimer.reset()
+    Logger.debug("Resetting: #{inspect result}")
+    {:noreply, socket}
+  end
+
+  def handle_event("pause", _value, socket) do
+    result = PomodoroTimer.pause()
+    Logger.debug("Pausing: #{inspect result}")
+    {:noreply, socket}
+  end
+
+  def handle_event("rest", _value, socket) do
+    result = PomodoroTimer.rest()
+    Logger.debug("Resting: #{inspect result}")
+    {:noreply, socket}
+  end
+
+
+  def handle_event(event, _value, socket) do
+    Logger.warn("Unhandled event: #{inspect event}")
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
   def handle_info({:pomodoro_timer, pomodoro_timer}, socket) do
-    socket = assign(socket, seconds_remaining: pomodoro_timer.seconds_remaining)
+    socket = assign(socket, timer: pomodoro_timer)
 
     {:noreply, socket}
   end
