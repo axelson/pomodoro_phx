fb426c4d1e486de059090b20565882390dc4f0ba
Jason Axelson <jason.axelson@gmail.com>
2024-08-21 07:09:54 -0500

    Update many deps

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule PomodoroPhx.MixProject do
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
+      {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
