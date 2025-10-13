06030c78287f13d3457457fa4270ca35dbd3089a
Jason Axelson <jason.axelson@gmail.com>
2025-10-12 18:28:48 -1000

    base on the last pomodoro version

--- mix.exs
+++ mix.exs
@@ -36,8 +36,8 @@ defmodule PomodoroPhx.MixProject do
   defp deps do
     [
       {:nimble_csv, ">= 0.0.0"},
-      {:pomodoro, path: "~/dev/pomodoro"},
-      # {:pomodoro, github: "axelson/pomodoro"},
+      # {:pomodoro, path: "~/dev/pomodoro"},
+      {:pomodoro, github: "axelson/pomodoro"},
       {:torch, "~> 5.3"},
       {:data_tracer, "~> 0.1.2", only: :dev},
       {:exsync, "~> 0.4.1", only: :dev},
